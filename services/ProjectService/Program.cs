fad1fb9b7607bf044a5c98ad5b6f05f87ce1721d
Uma19-proj <umanidya@gmail.com>
2025-11-14 16:45:12 +0530

    chore: remove misplaced ProjectService folder and fix solution references

--- services/ProjectService/Program.cs
+++ services/ProjectService/Program.cs
@@ -1,4 +1,4 @@
-//var builder = WebApplication.CreateBuilder(args);
+﻿//var builder = WebApplication.CreateBuilder(args);
 
 //// Add services to the container.
 //// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -39,4 +39,40 @@
 //{
 //    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 //}
+
 using Shared.Contracts.Models;
+using Microsoft.OpenApi.Models;  // ✅ Required for Swagger
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Project Service API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Example use of Shared.Contracts.Models
+var response = new BaseResponse
+{
+    Success = true,
+    Message = "Shared library reference works!"
+};
+
+app.Run();
